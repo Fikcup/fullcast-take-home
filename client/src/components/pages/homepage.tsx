8f43284d49b25356ecf047a5e9b87dad3696bf76
Fikcup <rhys.n.wright@gmail.com>
2024-02-10 13:22:29 -0800

    feat: homepage display

--- client/src/components/pages/homepage.tsx
+++ client/src/components/pages/homepage.tsx
@@ -1,46 +1,19 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 
-import { SortedCategory } from "../../types/Category";
-import AccordionCategory from "../molecules/AccordionCategory";
+import "./homepage.css";
+import CategoryList from "../organisms/CategoryList";
 
 const Homepage = () => {
-    const [categories, setCategories] = useState<SortedCategory[]>([]);
-
-    useEffect(() => {
-        const APP_URL = process.env.REACT_APP_APP_URL;
-        console.log(APP_URL);
-        const apiCalls = async () => {
-            await axios({
-                method: "GET",
-                url: APP_URL + "/api/categories", 
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
-                .then(async (res) => {
-                    const categoriesResponse: SortedCategory[] = await res.data;            
-                    setCategories(categoriesResponse);
-                })
-                .catch((err) => {
-                    console.error(err);
-                });
-        };
-
-        apiCalls();
-    }, []);
 
     return (
-        <div>
-            {
-                categories.map((category, index) => (
-                    <AccordionCategory 
-                        category={category} 
-                        key={`${index}-${category.categoryId}`}
-                    />
-                ))
-            }
-        </div>
+        <main className="main">
+            <section className="category-list">
+                <CategoryList />
+            </section>
+            <section className="notes">
+
+            </section>
+        </main>
     );
 };
 
